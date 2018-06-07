06e802dbb5ad5a14a7713e2b1cc3266880eb0b0e
skarp <skarp@SKARP-PC>
2018-06-07 13:40:27 +0100

    Redirect stderr to stdout

--- src/linuxacademy/git/gitStuff.groovy
+++ src/linuxacademy/git/gitStuff.groovy
@@ -1,7 +1,7 @@
 package linuxacademy.git;
 
 def gitCommit(gitPath) {
-	def cmd = "/bin/git --git-dir='${gitPath}' rev-parse HEAD"
+	def cmd = "/bin/git --git-dir='${gitPath}' rev-parse HEAD 2>&1"
 	// def cmd = "git --version"
 	return  cmd.execute().text.length()
 }
