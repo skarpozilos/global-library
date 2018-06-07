79214e5697b5af3253ab2bbaf7321df62ab8c781
skarp <skarp@SKARP-PC>
2018-06-07 13:16:47 +0100

    change .execute().text to .execute().toString().trim()

--- src/linuxacademy/git/gitStuff.groovy
+++ src/linuxacademy/git/gitStuff.groovy
@@ -1,6 +1,6 @@
 package linuxacademy.git;
 
 def gitCommit(gitPath) {
-	def cmd = "gita --git-dir=\"${gitPath}\" rev-parse HEAD"
-	return  cmd.execute().text
+	def cmd = "git --git-dir=\"${gitPath}\" rev-parse HEAD"
+	return  cmd.execute().toString().trim()
 }
