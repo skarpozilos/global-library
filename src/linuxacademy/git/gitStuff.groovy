dc32493825ea35aae391ac713be8a44b30f67976
skarp <skarp@SKARP-PC>
2018-06-07 13:29:00 +0100

    test git-dir with single quotes

--- src/linuxacademy/git/gitStuff.groovy
+++ src/linuxacademy/git/gitStuff.groovy
@@ -1,7 +1,7 @@
 package linuxacademy.git;
 
 def gitCommit(gitPath) {
-	// def cmd = "git --git-dir=\"${gitPath}\" rev-parse HEAD"
-	def cmd = "git --version"
+	def cmd = "/bin/git --git-dir='${gitPath}' rev-parse HEAD"
+	// def cmd = "git --version"
 	return  cmd.execute().text.trim()
 }
