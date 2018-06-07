32171a13ca84d96d6c6f5788c9e002443ad1bec9
skarp <skarp@SKARP-PC>
2018-06-07 13:38:48 +0100

    Try output variable length

--- src/linuxacademy/git/gitStuff.groovy
+++ src/linuxacademy/git/gitStuff.groovy
@@ -1,8 +1,7 @@
 package linuxacademy.git;
 
 def gitCommit(gitPath) {
-	// def cmd = "/bin/git --git-dir='${gitPath}' rev-parse HEAD"
-	def cmd = "cat '${gitPath}'/.git/HEAD"
+	def cmd = "/bin/git --git-dir='${gitPath}' rev-parse HEAD"
 	// def cmd = "git --version"
-	return  cmd.execute().text.trim()
+	return  cmd.execute().text.length()
 }
