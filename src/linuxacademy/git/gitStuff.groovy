c8f434d456e3daa399094705e690b637d2067297
skarp <skarp@SKARP-PC>
2018-06-07 13:48:26 +0100

    echo maybe?

--- src/linuxacademy/git/gitStuff.groovy
+++ src/linuxacademy/git/gitStuff.groovy
@@ -3,6 +3,6 @@ package linuxacademy.git;
 def gitCommit(gitPath) {
 	// def cmd = "/bin/git --git-dir='${gitPath}' rev-parse HEAD"
 	// def cmd = "git --version"
-	def cmd = "ls -l ${gitPath}"
-	return  cmd.execute().text
+	def cmd = "echo ${gitPath}"
+	return cmd.execute().text
 }
