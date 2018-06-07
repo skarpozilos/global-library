180b41959a479eee3c0f1b564aab76b49ecb5852
skarp <skarp@SKARP-PC>
2018-06-07 13:25:10 +0100

    test git --version

--- src/linuxacademy/git/gitStuff.groovy
+++ src/linuxacademy/git/gitStuff.groovy
@@ -1,6 +1,7 @@
 package linuxacademy.git;
 
 def gitCommit(gitPath) {
-	def cmd = "git --git-dir=\"${gitPath}\" rev-parse HEAD"
+	// def cmd = "git --git-dir=\"${gitPath}\" rev-parse HEAD"
+	def cmd = "git --version"
 	return  cmd.execute().text.trim()
 }
